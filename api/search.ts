d56660390034710c6906edeabdcc458f6dae27d4
Almeida <almeidx@pm.me>
2021-01-06 22:41:18 +0000

    Update search.ts

--- api/search.ts
+++ api/search.ts
@@ -3,7 +3,7 @@ import mdn from '../util/mdn';
 
 export default async function search(req: NowRequest, res: NowResponse) {
   const { q } = req.query;
-  if (!q) return res.status(400).json({ message: 'You need to pass the search query using the q parameter' });
+  if (!q) return res.status(400).json({ message: 'Missing ?q parameter' });
 
   const data = await mdn.search(q as string);
   if (!Array.isArray(data) || !data.length) return res.status(404).json({ message: 'Could not find anything' });
